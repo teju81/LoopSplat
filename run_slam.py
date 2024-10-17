8252ff31356e9383e38e45cc38b5f09f9284bc04
teju81 <teju81@gmail.com>
2024-10-17 19:15:56 +0530

    Update run_slam.py

--- run_slam.py
+++ run_slam.py
@@ -90,26 +90,26 @@ if __name__ == "__main__":
     config = load_config(args.config_path)
     config = update_config_with_args(config, args)
 
-    if os.getenv('DISABLE_WANDB') == 'true':
-        config["use_wandb"] = False
-    if config["use_wandb"]:
-        wandb.init(
-            project=config["project_name"],
-            config=config,
-            dir="/home/yli3/scratch/outputs/slam/wandb",
-            group=config["data"]["scene_name"]
-            if not args.group_name
-            else args.group_name,
-            name=f'{config["data"]["scene_name"]}_{time.strftime("%Y%m%d_%H%M%S", time.localtime())}_{str(uuid.uuid4())[:5]}',
-        )
-        wandb.run.log_code(".", include_fn=lambda path: path.endswith(".py"))
+    # if os.getenv('DISABLE_WANDB') == 'true':
+    #     config["use_wandb"] = False
+    # if config["use_wandb"]:
+    #     wandb.init(
+    #         project=config["project_name"],
+    #         config=config,
+    #         dir="/home/yli3/scratch/outputs/slam/wandb",
+    #         group=config["data"]["scene_name"]
+    #         if not args.group_name
+    #         else args.group_name,
+    #         name=f'{config["data"]["scene_name"]}_{time.strftime("%Y%m%d_%H%M%S", time.localtime())}_{str(uuid.uuid4())[:5]}',
+    #     )
+    #     wandb.run.log_code(".", include_fn=lambda path: path.endswith(".py"))
 
     setup_seed(config["seed"])
     gslam = GaussianSLAM(config)
     gslam.run()
 
-    evaluator = Evaluator(gslam.output_path, gslam.output_path / "config.yaml")
-    evaluator.run()
-    if config["use_wandb"]:
-        wandb.finish()
+    # evaluator = Evaluator(gslam.output_path, gslam.output_path / "config.yaml")
+    # evaluator.run()
+    # if config["use_wandb"]:
+    #     wandb.finish()
     print("All done.✨")
